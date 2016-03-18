d5ae9f8c03d8581d4b96737d0dd6a02b69099f48
jiemizc <zhouchang@pku.edu.cn>
2016-03-18 16:15:19 +0800

    clean target, add logs to track partition computation, add configuration options to halt computation when memory is low

--- giraph-1.0.0/giraph-core/target/munged/main/org/apache/giraph/comm/messages/InMemMessagePair.java
+++
@@ -1,9 +0,0 @@
-package org.apache.giraph.comm.messages;
-
-import org.apache.giraph.utils.ExtendedDataOutput;
-import org.apache.hadoop.io.WritableComparable;
-
-public class InMemMessagePair<I extends WritableComparable> {
-	public I id;
-	public ExtendedDataOutput msg;
-}
